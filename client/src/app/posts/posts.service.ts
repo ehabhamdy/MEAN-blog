a235d485978bc5045042ba92ffd11c3febb6b3a6
ehabhamdy2012 <ehabhamdy2012@gmail.com>
2020-12-27 16:22:00 +0100

    Navigate User to Post List after creating or updating a post

--- client/src/app/posts/posts.service.ts
+++ client/src/app/posts/posts.service.ts
@@ -3,13 +3,14 @@ import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { HttpClient } from '@angular/common/http'
 import { map } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Injectable({providedIn: 'root'})
 export class PostsService {
     private posts: Post[] = [];
     private postsUpdated = new Subject<Post[]>()
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private router: Router) {
 
     }
 
@@ -47,6 +48,7 @@ export class PostsService {
             post.id = responseData.postId;
             this.posts.push(post) 
             this.postsUpdated.next([... this.posts])
+            this.router.navigate(["/"])
         });
     }
 
@@ -68,6 +70,7 @@ export class PostsService {
             updatedPosts[oldPostIndex] = post;
             this.posts = updatedPosts;
             this.postsUpdated.next([...this.posts])
+            this.router.navigate(["/"])
         })
     }
 }
