c21b5bfb5b007800817b87c688fd6487de8b60a3
ehabhamdy2012 <ehabhamdy2012@gmail.com>
2020-12-29 17:56:06 +0100

    Login component makes request to the login route and recieve a token

--- client/src/app/auth/auth.service.ts
+++ client/src/app/auth/auth.service.ts
@@ -1,22 +1,36 @@
-import { HttpClient } from "@angular/common/http";
-import { Injectable } from "@angular/core";
-import { AuthData } from "./auth-data.model";
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { AuthData } from './auth-data.model';
 
 @Injectable({
-    providedIn: "root"
+  providedIn: 'root',
 })
 export class AuthService {
-    constructor(private http: HttpClient){}
+  constructor(private http: HttpClient) {}
 
-    createUser(email: string, password: string) {
-        console.log('sadfasdgf');
-        const authData: AuthData = {
-            email: email,
-            password: password
-        };
-        this.http.post("http://localhost:9000/api/user/signup", authData)
-            .subscribe(response => {
-                console.log(response);
-            });
-    }
+  createUser(email: string, password: string) {
+    console.log('sadfasdgf');
+    const authData: AuthData = {
+      email: email,
+      password: password,
+    };
+    this.http
+      .post('http://localhost:9000/api/user/signup', authData)
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
+
+  login(email: string, password: string) {
+    const authData: AuthData = {
+        email: email,
+        password: password,
+      };
+
+    this.http
+      .post('http://localhost:9000/api/user/login', authData)
+      .subscribe((response) => {
+        console.log(response); // This will contain the JWT token
+      });
+  }
 }
