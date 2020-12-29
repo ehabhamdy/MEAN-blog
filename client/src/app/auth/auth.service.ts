e37c46de26a789a47774f7cbabbeaaf376652fb9
ehabhamdy2012 <ehabhamdy2012@gmail.com>
2020-12-29 15:15:09 +0100

    Enabling the signup screen to create a new user

---
+++ client/src/app/auth/auth.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { AuthData } from "./auth-data.model";
+
+@Injectable({
+    providedIn: "root"
+})
+export class AuthService {
+    constructor(private http: HttpClient){}
+
+    createUser(email: string, password: string) {
+        console.log('sadfasdgf');
+        const authData: AuthData = {
+            email: email,
+            password: password
+        };
+        this.http.post("http://localhost:9000/api/user/signup", authData)
+            .subscribe(response => {
+                console.log(response);
+            });
+    }
+}
