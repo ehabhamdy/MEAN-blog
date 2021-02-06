eaf6a99d969d4e1bb71420ea82d832286814b3b3
ehabhamdy2012 <ehabhamdy2012@gmail.com>
2021-02-06 11:32:05 +0100

    Adjust posts per page in the posts list component

--- client/src/app/posts/post-list/post-list.component.ts
+++ client/src/app/posts/post-list/post-list.component.ts
@@ -21,9 +21,9 @@ export class PostListComponent implements OnInit, OnDestroy {
   posts: Post[] = [];
   isLoading = false;
   totalPosts = 0;
-  postsPerPage = 1;
+  postsPerPage = 5;
   currentPage = 1;
-  pageSizeOptions = [1, 2, 5, 10];
+  pageSizeOptions = [2, 5, 10];
   userIsAuthenticated = false;
   userId: string;
 
